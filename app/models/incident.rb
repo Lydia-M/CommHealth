35a3c40d1178807dd447895a3fce39746b2a6c96
Lydia Mansfield <lydia.m.mansfield@gmail.com>
2017-11-12 22:34:27 +0000

    Installed Geocoder and concatenated the address input. Having issues with saving geocoded lat-longs to table.

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,2 +1,12 @@
 class Incident < ApplicationRecord
+    geocoded_by :full_street_address, :latitude  => @latitude, :longitude => @longitude   # can also be an IP address
+    after_validation :geocode          # auto-fetch coordinates
+    
+    
+    
+    
+    def full_street_address
+       return [@street_address_1, @street_address_2, @city, @state, @zip].compact.reject(&:empty?).join(', ')
+    end
+    
 end
